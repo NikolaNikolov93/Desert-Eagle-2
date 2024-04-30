2171ad758125b4299e31937bc6051272f54dd5ef
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-05-01 00:42:35 +0300

    Added hero movement with keyboard

--- src/Game/hero/Hero.ts
+++ src/Game/hero/Hero.ts
@@ -1,6 +1,6 @@
 import * as PIXI from "pixi.js";
 PIXI.Assets.add({ alias: "hero1", src: "static/hero/planeRed1.png" });
-PIXI.Assets.add({ alias: "hero2", src: "static/hero/planeRed2.png" });
+PIXI.Assets.add({ alias: "hero3", src: "static/hero/planeRed3.png" });
 PIXI.Assets.add({ alias: "hero3", src: "static/hero/planeRed3.png" });
 
 interface App {
@@ -10,17 +10,15 @@ interface App {
 export default class Hero {
   private app: App;
   private assets: PIXI.Texture[];
+  private hero: any;
 
   constructor({ app }: { app: App }) {
     this.app = app;
     this.assets = [];
+    this.hero;
   }
   async loadAssets() {
-    const heroGraphics = await PIXI.Assets.load([
-      "hero1",
-      "hero2",
-      "hero3",
-    ]).then((graphics) => {
+    await PIXI.Assets.load(["hero1", "hero3", "hero3"]).then((graphics) => {
       for (const key in graphics) {
         this.assets.push(graphics[key]);
       }
@@ -31,7 +29,23 @@ export default class Hero {
     animatedPlane.zIndex = 1;
     animatedPlane.animationSpeed = 0.4;
     animatedPlane.play();
+    this.hero = animatedPlane;
     this.app.stage.addChild(animatedPlane);
   }
-  update() {}
+  move(direction: string) {
+    switch (direction) {
+      case "ArrowUp":
+        this.hero.y -= 3;
+        break;
+      case "ArrowDown":
+        this.hero.y += 3;
+        break;
+      case "ArrowLeft":
+        this.hero.x -= 3;
+        break;
+      case "ArrowRight":
+        this.hero.x += 3;
+        break;
+    }
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -19,12 +19,50 @@ if (appCanvas) {
  */
 let background = new Background({ app });
 background.loadAssets();
+
+/**
+ * Initialize hero by calling Hero class
+ */
 let hero = new Hero({ app });
 hero.loadAssets();
+
+/**
+ * Hero movement logic
+ */
+interface Keys {
+  [key: string]: boolean;
+}
+
+let keys: Keys = {};
+
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
+
+function keyDown(e: any) {
+  keys[e.code] = true;
+}
+
+function keyUp(e: any) {
+  keys[e.code] = false;
+}
+
 /**
  * Game Loop
  */
 app.ticker.add(() => {
+  //Background animation call
   background.update();
-  hero.update();
+  //Listen for her movement and updating the position
+  if (keys["ArrowUp"]) {
+    hero.move("ArrowUp");
+  }
+  if (keys["ArrowDown"]) {
+    hero.move("ArrowDown");
+  }
+  if (keys["ArrowLeft"]) {
+    hero.move("ArrowLeft");
+  }
+  if (keys["ArrowRight"]) {
+    hero.move("ArrowRight");
+  }
 });
