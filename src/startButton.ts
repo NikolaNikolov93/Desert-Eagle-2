c4265de539a6ada6189bb7a0414bda9546109d26
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-05-01 15:42:04 +0300

    added [styles]

--- src/startButton.ts
+++ src/startButton.ts
@@ -1,14 +1,19 @@
 /**
- *
+ * On start game button click --> Add the game script to the head component
+ *  and remove start game screen from the body
  */
+
+// Select the start game button
 const startButton = document.getElementById("start-game-button");
 
+// Add click handler
 startButton?.addEventListener("click", () => {
   const script = document.createElement("script");
   script.src = "/src/index.ts";
   script.type = "module";
   script.id = "game";
 
+  //Add the script and remove the start game screen
   document.head.appendChild(script);
   const startGameScreen: any = document.getElementById("start-game-screen");
   document.body.removeChild(startGameScreen);
